5737d082175c95ffbef7329f2d6f4f81344b50d7
frog016 <mixalyov.nikita@mail.ru>
2024-10-02 19:46:40 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JavaNaumenHW3
+ 
---
+++ src/main/java/org/example/JavaNaumenHW3/JavaNaumenHw3Application.java
@@ -0,0 +1,13 @@
+package org.example.JavaNaumenHW3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNaumenHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNaumenHw3Application.class, args);
+	}
+
+}
