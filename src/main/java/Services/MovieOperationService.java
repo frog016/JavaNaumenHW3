10b044dd26c506aebae633adf08d634800d61cc4
frog016 <mixalyov.nikita@mail.ru>
2024-10-03 15:54:02 +0500

    Реализовал бизнес логику по работе с фильмами

---
+++ src/main/java/Services/MovieOperationService.java
@@ -0,0 +1,40 @@
+package Services;
+
+import Cinema.Movie;
+import Database.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieOperationService implements MovieService {
+    private final MovieRepository repository;
+
+    @Autowired
+    public MovieOperationService(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void createMovie(Long id, int genre, float duration, String description) {
+        Movie movie = new Movie(id, genre, duration, description);
+        repository.create(movie);
+    }
+
+    @Override
+    public Movie findById(Long id) {
+        return repository.read(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.delete(id);
+    }
+
+    @Override
+    public void updateDescription(Long id, String description) {
+        Movie oldMovie = repository.read(id);
+        Movie newMovie = new Movie(id, oldMovie.getGenre(), oldMovie.getDuration(), description);
+
+        repository.update(newMovie);
+    }
+}
---
+++ src/main/java/Services/MovieService.java
@@ -0,0 +1,14 @@
+package Services;
+
+import Cinema.Movie;
+import Database.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public interface MovieService {
+    void createMovie(Long id, int genre, float duration, String description);
+    Movie findById(Long id);
+    void deleteById(Long id);
+    void updateDescription(Long id, String description);
+}
+
