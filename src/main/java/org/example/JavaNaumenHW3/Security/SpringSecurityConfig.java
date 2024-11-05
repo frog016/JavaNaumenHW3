68979928e7dd011f0d9d4f5da70b7925826d4e8a
frog016 <mixalyov.nikita@mail.ru>
2024-11-05 08:32:48 +0500

    Добавил класс конфигурации

---
+++ src/main/java/org/example/JavaNaumenHW3/Security/SpringSecurityConfig.java
@@ -0,0 +1,9 @@
+package org.example.JavaNaumenHW3.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+}
