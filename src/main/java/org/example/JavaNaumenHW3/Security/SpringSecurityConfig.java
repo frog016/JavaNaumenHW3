e8684e14fd3058958910a017e8fa5c43ad349028
frog016 <mixalyov.nikita@mail.ru>
2024-11-05 08:34:07 +0500

    Переопределил кодировщик паролей

--- src/main/java/org/example/JavaNaumenHW3/Security/SpringSecurityConfig.java
+++ src/main/java/org/example/JavaNaumenHW3/Security/SpringSecurityConfig.java
@@ -1,9 +1,16 @@
 package org.example.JavaNaumenHW3.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
