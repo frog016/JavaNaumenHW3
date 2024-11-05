3cadb0056129fccaaf750dccd91f61690988c070
frog016 <mixalyov.nikita@mail.ru>
2024-11-05 07:01:22 +0500

    Добавил аннотацию RepositoryRestResource
    Добавил аннотацию RepositoryRestResource для всех классов-репозиториев.

--- src/main/java/org/example/JavaNaumenHW3/Repository/MovieRepository.java
+++ src/main/java/org/example/JavaNaumenHW3/Repository/MovieRepository.java
@@ -2,9 +2,11 @@ package org.example.JavaNaumenHW3.Repository;
 
 import org.example.JavaNaumenHW3.Entity.Movie;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "movies")
 public interface MovieRepository extends CrudRepository<Movie, Long> {
     List<Movie> findByGenreAndTitle(String genre, String title);
 }
